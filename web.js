89645907b9ec77bb4cbdf01142f78a6eccd7ac81
Dominique Bosselmann <bosselmann@co2-filter.com>
2013-07-09 21:06:39 +0000

    Corrected output string

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   var buffer = fs.readFileSync("index.html");
   var output = buffer.toString("utf-8");
-  response.send(output); 
+  response.send("Vermutlich is nix drin: " + output); 
 });
 
 var port = process.env.PORT || 5000;
