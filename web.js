5c0abb75da140181a0f69ce01e01a1884d40e126
Dominique Bosselmann <bosselmann@co2-filter.com>
2013-07-09 20:56:49 +0000

    Added filesystem module

--- web.js
+++ web.js
@@ -1,11 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var buffer = fs.readFileSync("index.html");
   var output = buffer.toString("utf-8");
-  request.send(output);
+  response.send(output); 
 });
 
 var port = process.env.PORT || 5000;
